aa0565997b1c3c08774cce0e15e7cea21ec53c85
Nick Kruckenberg <nkruckenberg@gmail.com>
2020-08-20 17:33:51 -0400

    Merged changes with base

--- filesToServe/webpack-config.js
+++ filesToServe/webpack-config.js
@@ -2,9 +2,43 @@ const path = require('path');
 
 
 module.exports = {
-  entry: ./src/index.js,
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
   }
 }
